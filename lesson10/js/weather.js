35471b487975a331157e06901d9bae7720629588
Reed-Sarah <ree16014@byui.edu>
2020-03-14 16:00:50 -0700

    fixed windchill

--- lesson10/js/weather.js
+++ lesson10/js/weather.js
@@ -10,15 +10,19 @@ fetch(apiURL)
     document.getElementById('humidity').textContent = jsObject.main.humidity;
     document.getElementById('current').textContent = jsObject.weather[0].description;
     document.getElementById('temp').textContent = temp.toPrecision(3);
-  });
+ 
 
     if (windSpeed >= 3 && temp <= 50)
+    {
 var windChill = (35.74 + .6215 * temp - 35.75 * windSpeed ** .16 + .4275 * temp * windSpeed ** .16).toPrecision(3) + "&#00176;F";
+    }
 else
+{
 var windChill = "N/A";
+}
 
 document.getElementById("windChill").innerHTML = windChill;
-
+ });
 /* forecast */
 var apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=d362c4077b18b4682fc44e89fb3992b7&units=imperial"
 fetch(apiURL)
@@ -34,6 +38,7 @@ fetch(apiURL)
     {
        var dayId = "day" + j;
        console.log(day)
+       
         document.getElementById(dayId).textContent = week[((day + j) % 7)]; 
         
       
